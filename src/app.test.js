1cfd66bcc50dc2f1a604cd0570a12935fec13c32
Joe Turner <joe@oampo.co.uk>
2018-03-19 17:36:03 +0000

    Add render test

--- src/app.test.js
+++ src/app.test.js
@@ -8,6 +8,18 @@ describe('<App />', () => {
         shallow(<App />);
     });
 
-    it('Should render the items in the todo list');
+    it('Should render the items in the todo list', () => {
+        const items = ['Test 1', 'Test 2', 'Test 3'];
+        const wrapper = shallow(<App />);
+        wrapper.setState({
+            items
+        });
+
+        const lis = wrapper.find('.todo-item');
+        expect(lis.length).toEqual(items.length);
+        items.forEach((item, index) => {
+            expect(lis.at(index).text()).toEqual(items[index]);
+        });
+    });
     it('Should add an item when the form is submitted');
 });
