c3dca9915aee6f14c79dc3907b5d7990fa5aaf30
Joe Turner <joe@oampo.co.uk>
2018-03-19 11:57:36 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: ['Buy milk', 'Learn React', 'Profit']
+        };
+    }
+
+    addItem(e) {
+        e.preventDefault();
+        this.setState({
+            items: [...this.state.items, this.input.value]
+        });
+    }
+
+    render() {
+        const items = this.state.items.map((item, index) => (
+            <li className="todo-item" key={index}>
+                {item}
+            </li>
+        ));
+        return (
+            <div className="app">
+                <ul className="todo-list">{items}</ul>
+
+                <form className="add-form" onSubmit={e => this.addItem(e)}>
+                    <label htmlFor="textInput">Item to add</label>
+                    <input
+                        type="text"
+                        id="textInput"
+                        ref={element => (this.input = element)}
+                    />
+                    <button>Add item</button>
+                </form>
+            </div>
+        );
+    }
+}
---
+++ src/app.test.js
@@ -0,0 +1,7 @@
+import App from './app';
+
+describe('<App />', () => {
+    it('Should renderer without crashing');
+    it('Should render the items in the todo list');
+    it('Should add an item when the form is submitted');
+});
