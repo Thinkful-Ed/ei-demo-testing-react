eca29b402313520b35df30cc7628392ebe43b5d4
Joe Turner <joe@oampo.co.uk>
2018-03-20 11:11:37 +0000

    Add interactivity test

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import App from './app';
 
@@ -21,5 +21,25 @@ describe('<App />', () => {
             expect(lis.at(index).text()).toEqual(items[index]);
         });
     });
-    it('Should add an item when the form is submitted');
+
+    it('Should add an item when the form is submitted', () => {
+        const items = ['Test 1', 'Test 2', 'Test 3'];
+        const toAdd = 'Test 4';
+        const wrapper = mount(<App />);
+        wrapper.setState({
+            items
+        });
+
+        const input = wrapper.find('#textInput');
+        input.instance().value = toAdd;
+
+        const form = wrapper.find('.add-form');
+        const preventDefault = jest.fn();
+        form.simulate('submit', {
+            preventDefault
+        });
+
+        expect(wrapper.state('items')).toEqual([...items, toAdd]);
+        expect(preventDefault).toHaveBeenCalled();
+    });
 });
